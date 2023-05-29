91cdd9ca5d0f71ea7fac6355ebcf9b530a392e06
Kike Paez <enrique.paezpozo.com>
2023-05-29 18:40:19 +0200

    Task component created and added with style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import FreecodecampLogo from './components/FreecodecampLogo';
+import Task from './components/Task';
 
 function App() {
   return (
     <div className="App">
       <FreecodecampLogo />
+      <div className='main-task-list'>
+        <h1>My Tasks</h1>
+        <Task text='Learn React'></Task>
+      </div>
     </div>
   );
 }
